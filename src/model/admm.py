ff9d94c873cf68fc55b7be9625d63a591a0901db
Aidar Khasanov <arkhasanov_1@edu.hse.ru>
2026-06-15 13:30:04 +0300

    upd

---
+++ src/model/admm.py
@@ -0,0 +1,45 @@
+from torch import nn
+from torch.nn import Sequential
+
+
+class ADMM(nn.Module):
+    """
+    ADMM implementation from [paper](https://arxiv.org/pdf/1908.11502)
+    """
+
+    def __init__(self):
+        """
+        Args:
+            n_feats (int): number of input features.
+            n_class (int): number of classes.
+            fc_hidden (int): number of hidden features.
+        """
+        super().__init__()
+
+        
+
+    def forward(self, data_object, **batch):
+        """
+        Model forward method.
+
+        Args:
+            data_object (Tensor): input vector.
+        Returns:
+            output (dict): output dict containing logits.
+        """
+        return {"logits": self.net(data_object)}
+
+    def __str__(self):
+        """
+        Model prints with the number of parameters.
+        """
+        all_parameters = sum([p.numel() for p in self.parameters()])
+        trainable_parameters = sum(
+            [p.numel() for p in self.parameters() if p.requires_grad]
+        )
+
+        result_info = super().__str__()
+        result_info = result_info + f"\nAll parameters: {all_parameters}"
+        result_info = result_info + f"\nTrainable parameters: {trainable_parameters}"
+
+        return result_info
